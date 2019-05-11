7a0bde8617b9189e411589041dceb6b72cd531aa
Jonathan Godbout <jon.godbout@gmail.com>
2019-05-10 21:48:37 -0400

    Initialized empty project

---
+++ src/PointCloudTemplateMatching.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+#include <math.h>
+#include <stdlib.h>
+
+#include <string>
+#include <vector>
+
+#include <pcl/pcl_base.h>
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello" << std::endl;
+
+    return 0;
+}
+
